483e0b743fcb0c88af4f37c8247ab1e617c6a009
vladimir-mhl <vladimir.mhl@gmail.com>
2017-03-08 22:34:41 +0200

    added /orders js samples

---
+++ 2.0/javascript/orders/getOrder.js
@@ -0,0 +1,22 @@
+'strict'
+
+const CallfireClient = require('callfire-api-client-js');
+const client = new CallfireClient('api-login', 'api-password');
+
+client.ready(() => {
+    client.orders.getOrder({
+      id: 379506003,
+      // return only specific fields
+      fields: 'status,totalCost'
+    })
+      .then((response) => {
+        console.log(response.obj);
+      })
+      .catch(function (err) {
+        console.log('request error ' + err.data);
+      });
+  },
+  (clientError) => {
+    console.log('client error ' + clientError);
+  }
+);
---
+++ 2.0/javascript/orders/orderNumbers.js
@@ -0,0 +1,26 @@
+'strict'
+
+const CallfireClient = require('callfire-api-client-js');
+const client = new CallfireClient('api-login', 'api-password');
+
+client.ready(() => {
+    client.orders.orderNumbers({
+      body: {
+        // order particular numbers
+        numbers: ['12131234567', '12131234568']
+        // or find and order area-specific numbers
+        // localCount: '2',
+        // zipcode: '90401'
+      }
+    })
+      .then((response) => {
+        console.log(response.obj);
+      })
+      .catch(function (err) {
+        console.log('request error ' + err.data);
+      });
+  },
+  (clientError) => {
+    console.log('client error ' + clientError);
+  }
+);
