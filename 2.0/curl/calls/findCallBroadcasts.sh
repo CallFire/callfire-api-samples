fad5d5bfd45395fd3934863a91a5a046f45ecb0b
ZelNaga <svirhyn@gmail.com>
2018-03-28 17:49:18 +0300

    CF-18927 updated code examples to show for /me endpoint where we need to use not api creds but login/password
    CF-19063 updated find campaigns /v2/calls/broadcasts curl code example

--- 2.0/curl/calls/findCallBroadcasts.sh
+++ 2.0/curl/calls/findCallBroadcasts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -u username:password -H "Content-Type:application/json" -X GET "https://api.callfire.com/v2/calls/broadcasts?name=broadcast name&label=my label&running=true&offset=0&limit=10&fields=items(id,name,status,sounds)"
+curl -u username:password -H "Content-Type:application/json" -X GET "https://api.callfire.com/v2/calls/broadcasts?name=broadcast name&label=my label&running=true&offset=0&limit=10&fields=items(id,name,status,sounds)&scheduled=false&intervalBegin=1473781817000&intervalEnd=1473781817000"
--- 2.0/curl/me/sendVerificationCodeToCallerId.sh
+++ 2.0/curl/me/sendVerificationCodeToCallerId.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -u "username:password" -H "Content-Type:application/json" -X POST "https://api.callfire.com/v2/me/callerids/12132000384"
+curl -u "api_login:api_password" -H "Content-Type:application/json" -X POST "https://api.callfire.com/v2/me/callerids/12132000384"
