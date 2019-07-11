079df96be62a84dd65a42b0c0ffe4e6c640e1ea9
Vladimir Mikhailov <vmikhailov@callfire.com>
2019-07-11 17:47:13 +0300

    CF-15584 Added IVR config samples for PUT /numbers/config API

--- 2.0/java/numbers/updateNumberLeaseConfig.java
+++ 2.0/java/numbers/updateNumberLeaseConfig.java
@@ -14,7 +14,6 @@ class ApiClientSample {
         CallfireClient client = new CallfireClient("api_login", "api_password");
         NumberConfig config = new NumberConfig();
         config.setNumber("'19206596476'");
-        config.setConfigType(NumberConfig.ConfigType.TRACKING);
         // create call tracking config
         CallTrackingConfig callTrackingConfig = new CallTrackingConfig();
         callTrackingConfig.setScreen(false);
@@ -41,7 +40,57 @@ class ApiClientSample {
         googleAnalytics.setGoogleAccountId("UA - XXXXX - 2X");
         callTrackingConfig.setGoogleAnalytics(googleAnalytics);
 
+        // create IVR config
+        // IvrInboundConfig ivrInboundConfig = new IvrInboundConfig();
+        // ivrInboundConfig.setDialplanXml(buildDialplanXml());
+        // config.setIvrInboundConfig(ivrInboundConfig);
+        // config.setConfigType(NumberConfig.ConfigType.IVR);
+
+        // depends on what type of configuration your need assign TRACKING or IVR config
+        config.setConfigType(NumberConfig.ConfigType.TRACKING);
         config.setCallTrackingConfig(callTrackingConfig);
+        // update number configuration
         client.numberLeasesApi().updateConfig(config);
+    }
+
+    private static String buildDialplanXml() {
+        return
+            " <dialplan name=\"Root\">                                                                                        "
+                    + " <menu maxDigits=\"1\" timeout=\"3500\" name=\"Live\">                                                     "
+                    + "     <play type=\"tts\" voice=\"female1\">                                                                 "
+                    + "         Hello this is AB Advertising, please press 1 to transfer to our sales representative,              "
+                    + "         press 2 to contact clients support.                                                               "
+                    + "      </play>                                                                                              "
+                    + "      <keypress pressed=\"1\">                                                                             "
+                    + "          <!-- user pressed 1, store this data in 'selected_menu' variable -->                             "
+                    + "          <stash varname=\"selected_menu\">sales</stash>                                                   "
+                    + "          <play type=\"tts\" voice=\"female1\">                                                            "
+                    + "              You will be transferred to sales representative in a moment. Please wait.                    "
+                    + "          </play>                                                                                          "
+                    + "          <transfer callerid=\"${call.callerid}\" musiconhold=\"blues\" mode=\"ringall\">                  "
+                    + "              15550004455                                                                                  "
+                    + "          </transfer>                                                                                      "
+                    + "          </keypress>                                                                                      "
+                    + "          <keypress pressed=\"2\" name=\"kp_become_volonteer\">                                            "
+                    + "              <!-- user pressed 2, store this data in 'selected_menu' variable -->                         "
+                    + "              <stash varname=\"selected_menu\">support</stash>                                             "
+                    + "              <play type=\"tts\" voice=\"female1\">                                                        "
+                    + "                  You will be transferred to clients support. Please wait.                                 "
+                    + "              </play>                                                                                      "
+                    + "              <transfer callerid=\"${call.callerid}\" musiconhold=\"blues\" mode=\"ringall\">              "
+                    + "                  15550005500                                                                              "
+                    + "              </transfer>                                                                                  "
+                    + "           </keypress>                                                                                     "
+                    + "           <!-- if pressed key is not specified in menu replay Live menu -->                               "
+                    + "           <keypress pressed=\"default\" name=\"incorrect_Selection\">                                     "
+                    + "               <play type=\"tts\" voice=\"female1\">That is not a valid selection. Please try again.</play>"
+                    + "               <goto name=\"replay_Live\">Live</goto>                                                      "
+                    + "           </keypress>                                                                                     "
+                    + " </menu>                                                                                                   "
+                    + " <play type=\"tts\" voice=\"female1\">                                                                     "
+                    + "     Thank you for calling us. Have a good day.                                                            "
+                    + " </play>                                                                                                   "
+                    + " <hangup name=\"Hangup\"/>                                                                                 "
+                    + " </dialplan>                                                                                               ";
     }
 }
